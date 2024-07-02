cc0d68eb46d85c9365565c5ef3762b1f84037e6c
Abraham Kiplagat <kurerelagat01@.com>
2024-07-02 22:23:03 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+const firebaseConfig = FirebaseOptions(
+  apiKey: "AIzaSyAgqhQOlqRslHrEYWGfCS65X-q8V2rNnhs",
+  authDomain: "church-7c5aa.firebaseapp.com",
+  projectId: "church-7c5aa",
+  storageBucket: "church-7c5aa.appspot.com",
+  messagingSenderId: "629630311867",
+  appId: "1:629630311867:web:22450026f66a82dbcb789e",
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: firebaseConfig,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
