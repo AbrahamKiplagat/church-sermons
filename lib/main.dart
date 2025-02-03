f4cc9a500d6af2fdb24b06c1076de243d77d4ba3
Abraham Kiplagat <kurerelagat01@.com>
2025-02-03 19:04:35 +0300

    Would like to use mongo db instead of firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:app/views/screens/prayers/prayers_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:app/views/screens/dashboard/home_page.dart';
 import 'package:app/views/screens/blogs/blogs_page.dart'; // Import the EventsPage
 import 'package:app/views/screens/events/events_page.dart'; // Import the EventsPage
 import 'package:app/views/screens/sidebar/animated_sidebar.dart'; // Import the DrawerNavigation class
 
-const firebaseConfig = FirebaseOptions(
-  apiKey: "AIzaSyAgqhQOlqRslHrEYWGfCS65X-q8V2rNnhs",
-  authDomain: "church-7c5aa.firebaseapp.com",
-  projectId: "church-7c5aa",
-  storageBucket: "church-7c5aa.appspot.com",
-  messagingSenderId: "629630311867",
-  appId: "1:629630311867:web:22450026f66a82dbcb789e",
-);
-
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: firebaseConfig,
-  );
   runApp(MyApp());
 }
 
