bea157a632dfbd1d0f92454dadb7e17c71901e10
Abraham Kiplagat <kurerelagat01@.com>
2025-03-04 22:51:52 +0300

    nav

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Church Sermons App',
+      title: 'Church Sermons',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/views/screens/sidebar/animated_sidebar.dart
+++ lib/views/screens/sidebar/animated_sidebar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app/views/screens/prayers/prayers_screen.dart';
 import 'package:app/views/screens/events/events_page.dart'; // Import EventsPage
-import 'package:app/views/screens/blogs/blogs_page.dart'; // Import EventsPage
+import 'package:app/views/screens/blogs/blogs_page.dart'; // Import BlogsPage
+import 'package:app/views/screens/books/books_page.dart'; // Import BooksPage
 
 class DrawerNavigation extends StatelessWidget {
   @override
@@ -12,144 +13,133 @@ class DrawerNavigation extends StatelessWidget {
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 39, 39, 39),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'City Church',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.cancel, color: Colors.white),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                ],
-              ),
+            _buildDrawerHeader(context),
+            _buildListTile(
+              context,
+              icon: Icons.event,
+              title: 'Events',
+              routeName: EventsPage.routeName,
             ),
-            ListTile(
-              leading: Icon(Icons.event, color: Colors.white),
-              title: Text(
-                'Events',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                Navigator.of(context).pushNamed(EventsPage.routeName);
-              },
+            _buildListTile(
+              context,
+              icon: Icons.article,
+              title: 'Blogs',
+              routeName: BlogsPage.routeName,
             ),
-            ListTile(
-              leading: Icon(Icons.article, color: Colors.white), // Icon color
-              title: Text(
-                'Blogs',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                // Navigate to Blogs page or handle onTap event
-                 Navigator.of(context).pushNamed(BlogsPage.routeName);
-              },
+            _buildListTile(
+              context,
+              icon: Icons.chrome_reader_mode,
+              title: 'Prayers',
+              routeName: PrayersPage.routeName,
             ),
-            ListTile(
-              leading: Icon(Icons.chrome_reader_mode, color: Colors.white), // Icon color
-              title: Text(
-                'Prayers',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                // Navigate to Prayers page or handle onTap event
-                 Navigator.of(context).pushNamed(PrayersPage.routeName);
-              },
+            _buildListTile(
+              context,
+              icon: Icons.book,
+              title: 'Books',
+              routeName: BooksPage.routeName,
             ),
-            ListTile(
-              leading: Icon(Icons.book, color: Colors.white), // Icon color
-              title: Text(
-                'Books',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
+            _buildListTile(
+              context,
+              icon: Icons.image,
+              title: 'Gallery',
               onTap: () {
-                // Navigate to Books page or handle onTap event
                 Navigator.of(context).pop(); // Close the drawer
               },
             ),
-            ListTile(
-              leading: Icon(Icons.image, color: Colors.white), // Icon color
-              title: Text(
-                'Gallery',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
+            _buildListTile(
+              context,
+              icon: Icons.search,
+              title: 'Find Church',
               onTap: () {
-                // Navigate to Gallery page or handle onTap event
                 Navigator.of(context).pop(); // Close the drawer
               },
             ),
-            ListTile(
-              leading: Icon(Icons.search, color: Colors.white), // Icon color
-              title: Text(
-                'Find Church',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
+            _buildListTile(
+              context,
+              icon: Icons.business,
+              title: 'About',
               onTap: () {
-                // Navigate to Find Church page or handle onTap event
                 Navigator.of(context).pop(); // Close the drawer
               },
             ),
-            ListTile(
-              leading: Icon(Icons.business, color: Colors.white), // Icon color
-              title: Text(
-                'About',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
+            _buildListTile(
+              context,
+              icon: Icons.contact_mail,
+              title: 'Contact',
               onTap: () {
-                // Navigate to About page or handle onTap event
                 Navigator.of(context).pop(); // Close the drawer
               },
             ),
-            ListTile(
-              leading: Icon(Icons.contact_mail, color: Colors.white), // Icon color
-              title: Text(
-                'Contact',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                // Navigate to Contact page or handle onTap event
-                Navigator.of(context).pop(); // Close the drawer
-              },
-            ),
-            // Add other list tiles as needed
           ],
         ),
       ),
+    );
+  }
+
+  // Helper method to build the DrawerHeader
+  Widget _buildDrawerHeader(BuildContext context) {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 39, 39, 39),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Wrap the "City Church" text in a GestureDetector
+              GestureDetector(
+                onTap: () {
+                  // Navigate to the homepage
+                  Navigator.of(context).pop(); // Close the drawer
+                  Navigator.of(context).pushReplacementNamed('/'); // Replace with your homepage route
+                },
+                child: Text(
+                  'City Church',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.cancel, color: Colors.white),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 5),
+        ],
+      ),
+    );
+  }
+
+  // Helper method to build ListTile with navigation
+  Widget _buildListTile(
+    BuildContext context, {
+    required IconData icon,
+    required String title,
+    String? routeName,
+    VoidCallback? onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      onTap: onTap ?? () {
+        if (routeName != null) {
+          Navigator.of(context).pushNamed(routeName);
+        }
+      },
     );
   }
 }
